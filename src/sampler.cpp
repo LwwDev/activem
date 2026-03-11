3929b17eb06407cf75505d696546825df767e947
Liam <liam@wohlstedt.com>
2026-03-11 14:09:51 +0100

    fixed

--- src/sampler.cpp
+++ src/sampler.cpp
@@ -28,7 +28,7 @@ Metrics Sampler::sample(){
     uint64_t total = 0;
     size_t size = sizeof(total); 
     sysctlbyname("hw.memsize", &total, &size, nullptr, 0);
-    double totalMB = total/ 1024 / 1024;
+    double totalMB = total/ 1024.0 / 1024.0;
 
     vm_size_t pageSize;
     vm_statistics64_data_t vmStats;
@@ -37,7 +37,7 @@ Metrics Sampler::sample(){
     host_statistics64(mach_host_self(), HOST_VM_INFO64, (host_info64_t)&vmStats, &count);
 
 
-    double availMB = (vmStats.free_count * pageSize) / 1024/1024;
+    double availMB = (vmStats.free_count * pageSize) / 1024.0/1024.0;
     double usedMB = totalMB - availMB;
 
     return Metrics{ 42.0, usedMB, totalMB, 0};
